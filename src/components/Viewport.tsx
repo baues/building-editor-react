c8c6916e2f7b10c63b8bf85a68992e53194e60c9
kakemotokeita <keita.kakemoto@baues.io>
2021-10-09 12:50:10 +0900

    0.1.1

--- src/components/Viewport.tsx
+++ src/components/Viewport.tsx
@@ -6,9 +6,17 @@ const Viewport: React.VFC<React.HTMLAttributes<HTMLDivElement>> = (props) => {
   const { editor } = useEditor();
   useInit();
 
+  const toggleContextMenu = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (editor.contextMenu.open) {
+      editor.contextMenu.hide();
+    } else {
+      editor.contextMenu.show(e.clientX, e.clientY);
+    }
+  };
+
   return (
     <div id="building-editor-viewport" style={{ width: '100%', height: '100%' }} {...props}>
-      <div ref={(ref): HTMLCanvasElement | null => ref && ref.appendChild(editor.renderer.domElement)} />
+      <div ref={(ref): HTMLCanvasElement | null => ref && ref.appendChild(editor.renderer.domElement)} onContextMenu={toggleContextMenu} />
     </div>
   );
 };
